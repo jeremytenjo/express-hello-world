a016d188069339c76cfadfb0d8bf7828de9e3bda
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-08-30 15:24:46 -0400

    chore: Update app.js to add CORS headers and fetch jokes from external API

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const express = require("express");
+const express = require('express');
+const request = require('request');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => res.type('html').send(html));
+
+app.get('/jokes/random', (req, res) => {
+  request(
+    { url: 'https://www.fortnite.com/@bhe/8064-7152-2934?lang=en-US' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
@@ -58,4 +80,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
